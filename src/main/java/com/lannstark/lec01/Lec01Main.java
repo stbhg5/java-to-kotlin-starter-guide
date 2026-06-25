f04efa4ee1e1ac385c990c428b36ec62c4f7f893
jint <stbhg5@gmail.com>
2026-06-25 23:13:34 +0900

    수정

--- src/main/java/com/lannstark/lec01/Lec01Main.java
+++ src/main/java/com/lannstark/lec01/Lec01Main.java
@@ -5,12 +5,12 @@ import java.util.List;
 
 public class Lec01Main {
 
-  public static void main(String[] args) {
-    long number1 = 10L; // (1)
-    final long number2 = 10L; // (2)
+    public static void main(String[] args) {
+        long number1 = 10L; // (1)
+        final long number2 = 10L; // (2)
 
-    Long number3 = 1_000L; // (3)
-    Person person = new Person("최태현"); // (4)
-  }
+        Long number3 = 1_000L; // (3)
+        Person person = new Person("송진성"); // (4)
+    }
 
 }
